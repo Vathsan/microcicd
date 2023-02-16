86cc53c39e0789a5b5699ca1e201e0a0d1eb4116
Vathsan <srivathsanmail@gmail.com>
2023-02-16 09:42:04 -0700

    Remove target directory

--- build.py
+++ build.py
@@ -2,9 +2,7 @@
 from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
-use_plugin("python.unittest")
 use_plugin("python.flake8")
-use_plugin("python.coverage")
 use_plugin("python.distutils")
 
 
@@ -15,5 +13,4 @@ default_task = "publish"
 
 @init
 def set_properties(project):
-    project.set_property('coverage_break_build', False)
     pass
